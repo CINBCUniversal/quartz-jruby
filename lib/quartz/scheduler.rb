bc95ef637c1cca191faf51e3b90e751025562f29
Harry Alford and Stephen Ball <harrya+sball@pivotal.io>
2015-04-14 13:03:29 -0400

    Made changes to accommodate triggered jobs

--- lib/quartz/scheduler.rb
+++ lib/quartz/scheduler.rb
@@ -5,6 +5,8 @@ java_import 'org.quartz.impl.StdSchedulerFactory'
 java_import 'org.quartz.SimpleScheduleBuilder'
 java_import 'org.quartz.CronScheduleBuilder'
 
+java_import 'org.quartz.SchedulerException'
+
 module Quartz
   module Scheduler
     def self.included(base)
@@ -14,60 +16,95 @@ module Quartz
         include Singleton
       end
     end
-    
+
     module ClassMethods
       def schedule(name, options, &block)
         instance.schedule(name, options, block)
       end
+
+      def run_once(name, &block)
+        instance.run_once(name, block)
+      end
+
+
     end
-       
-    module InstanceMethods            
+
+
+    module InstanceMethods
+
+      def scheduler_factory
+        @scheduler_factory ||= StdSchedulerFactory.new
+      end
+
+      def scheduler
+        @scheduler ||= scheduler_factory.get_scheduler
+      end
+
+      def run
+        puts scheduler.getSchedulerName
+        if (scheduler.isStarted == false)
+          puts "starting...."
+          scheduler.start
+        end
+      end
+
+      def status
+        puts scheduler.getSchedulerName
+        scheduler.getCurrentlyExecutingJobs
+      end
+
+
+      def job_code_blocks
+        JobBlocksContainer.instance
+      end
+
       def schedule(name, options, block)
         job_code_blocks.jobs[name.to_s] = block
-        
+
         job_class = (options[:disallow_concurrent] ? Quartz::CronJobSingle : Quartz::CronJob)
-        job = JobBuilder.new_job(job_class.java_class).with_identity("#{name}", self.class.to_s).build  
-        
+        job = JobBuilder.new_job(job_class.java_class).with_identity("#{name}", self.class.to_s).build
+
         if options[:cron]
           trigger_schedule = CronScheduleBuilder.cron_schedule(options[:cron])
         else
           trigger_schedule = SimpleScheduleBuilder.simple_schedule.
                           with_interval_in_seconds(options[:every].to_i).repeat_forever
         end
-                                                                                                                             
+
         trigger = TriggerBuilder.new_trigger.with_identity("#{name}_trigger", self.class.to_s).
                           with_schedule(trigger_schedule).build
-                      
-        scheduler.schedule_job(job, trigger)                                                                                           
+
+        puts scheduler.getSchedulerName
+        scheduler.schedule_job(job, trigger)
       end
-  
-      def scheduler_factory
-        @scheduler_factory ||= StdSchedulerFactory.new
+
+      def run_once(name, block)
+        begin
+          job_code_blocks.jobs[name.to_s] = block
+          job_class = Quartz::CronJobSingle
+          job = JobBuilder.new_job(job_class.java_class).with_identity("#{name}", self.class.to_s).build
+
+          scheduler.addJob job, true
+
+          response = scheduler.trigger_job(job.key)
+        rescue SchedulerException => e
+          $stderr.print "exception: #{e} "
+        end
       end
-  
-      def scheduler
-        @scheduler ||= scheduler_factory.get_scheduler
-      end
-  
-      def job_code_blocks
-        JobBlocksContainer.instance
-      end
-  
-      def run
-        scheduler.start
-      end
-      
+
       def interrupt
-        scheduler.standby                                      # don't trigger new jobs        
+        scheduler.standby                                      # don't trigger new jobs
         scheduler.getCurrentlyExecutingJobs.each do |job_context|
           scheduler.interrupt(job_context.job_detail.key)      # interrupt job
-        end        
+        end
       end
-      
+
       def stop
         interrupt
         scheduler.shutdown(true)
       end
+
+
     end
   end
 end
